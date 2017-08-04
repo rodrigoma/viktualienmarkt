663556a22d97ae40703473ad55347276587cc0de
Rodrigo Monteiro <rodrigoma@gmail.com>
2017-08-04 15:33:21 -0300

    stop SlackBot

--- src/main/java/com/moip/hackday/SlackBot.java
+++ src/main/java/com/moip/hackday/SlackBot.java
@@ -50,7 +50,7 @@ public class SlackBot extends Bot {
      * @param session
      * @param event
      */
-    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
+    @Controller(events = {EventType.PIN_ADDED})
     public void onReceiveDM(WebSocketSession session, Event event) {
         reply(session, event, new Message("Hi, I am " + slackService.getCurrentUser().getName()));
     }
@@ -63,7 +63,7 @@ public class SlackBot extends Bot {
      * @param session
      * @param event
      */
-    @Controller(events = EventType.MESSAGE, pattern = "^([a-z ]{2})(\\d+)([a-z ]{2})$")
+    @Controller(events = EventType.PIN_ADDED, pattern = "^([a-z ]{2})(\\d+)([a-z ]{2})$")
     public void onReceiveMessage(WebSocketSession session, Event event, Matcher matcher) {
         reply(session, event, new Message("First group: " + matcher.group(0) + "\n" +
                 "Second group: " + matcher.group(1) + "\n" +
