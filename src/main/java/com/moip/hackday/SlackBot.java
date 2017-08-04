d7ff3cbaf1b5450936a67c2100e9ef9889d9c01a
Lucas Queiroz <lucas.queiroz@moip.com.br>
2017-08-04 15:29:07 -0300

    Remove Slack Bot

--- src/main/java/com/moip/hackday/SlackBot.java
+++ src/main/java/com/moip/hackday/SlackBot.java
@@ -8,7 +8,6 @@ import me.ramswaroop.jbot.core.slack.models.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.regex.Matcher;
@@ -20,7 +19,7 @@ import java.util.regex.Matcher;
  * @author ramswaroop
  * @version 1.0.0, 05/06/2016
  */
-@Component
+//@Component
 public class SlackBot extends Bot {
 
     private static final Logger logger = LoggerFactory.getLogger(SlackBot.class);
