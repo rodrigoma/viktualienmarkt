78d3c11e62678a70153ec3d5db8e0db4d249b0ee
Rodrigo Monteiro <rodrigoma@gmail.com>
2017-08-04 16:02:22 -0300

    save product

--- src/main/java/com/moip/hackday/BrenjoeiBot.java
+++ src/main/java/com/moip/hackday/BrenjoeiBot.java
@@ -3,22 +3,23 @@ package com.moip.hackday;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moip.hackday.domain.entity.Product;
+import com.moip.hackday.domain.repository.ProductRepository;
 import me.ramswaroop.jbot.core.slack.Bot;
 import me.ramswaroop.jbot.core.slack.Controller;
-import me.ramswaroop.jbot.core.slack.EventType;
 import me.ramswaroop.jbot.core.slack.models.Event;
 import me.ramswaroop.jbot.core.slack.models.Message;
 import me.ramswaroop.jbot.core.slack.models.RichMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static me.ramswaroop.jbot.core.slack.EventType.DIRECT_MESSAGE;
 
 @Component
 public class BrenjoeiBot extends Bot {
@@ -26,6 +27,9 @@ public class BrenjoeiBot extends Bot {
     private static final Logger logger = LoggerFactory.getLogger(BrenjoeiBot.class);
 
     private Map<String, Product> products = new HashMap<String, Product>();
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Value("${slackBotToken}")
     private String slackToken;
@@ -40,7 +44,7 @@ public class BrenjoeiBot extends Bot {
         return this;
     }
 
-    @Controller(pattern = "(quero vender)", next = "productName", events = {EventType.DIRECT_MESSAGE})
+    @Controller(pattern = "(quero vender)", next = "productName", events = {DIRECT_MESSAGE})
     public void sellProduct(WebSocketSession session, Event event) {
         logger.info("Quero vender " + products.size());
         logger.info("User null: " + (event.getUser() == null));
@@ -50,7 +54,7 @@ public class BrenjoeiBot extends Bot {
         reply(session, event, new Message("O que você quer vender?"));
     }
 
-    @Controller(next = "productPrice", events = {EventType.DIRECT_MESSAGE})
+    @Controller(next = "productPrice", events = {DIRECT_MESSAGE})
     public void productName(WebSocketSession session, Event event) {
         logger.info("Nome do produto " + products.size());
         logger.info("User null: " + (event.getUser() == null));
@@ -59,7 +63,7 @@ public class BrenjoeiBot extends Bot {
         nextConversation(event);
     }
 
-    @Controller(next = "productImage", events = {EventType.DIRECT_MESSAGE})
+    @Controller(next = "productImage", events = {DIRECT_MESSAGE})
     public void productPrice(WebSocketSession session, Event event) {
         logger.info("Qual o preço " + products.size());
         logger.info("User null: " + (event.getUser() == null));
@@ -68,45 +72,32 @@ public class BrenjoeiBot extends Bot {
         nextConversation(event);
     }
 
-    @Controller(next = "confirm", events = {EventType.DIRECT_MESSAGE})
+    @Controller(next = "confirm", events = {DIRECT_MESSAGE})
     public void productImage(WebSocketSession session, Event event) {
         logger.info("Adicionar imagem " + products.size());
         logger.info("User null: " + (event.getUser() == null));
+        Product product = getProduct(event);
         if (!event.getText().equalsIgnoreCase("não")) {
-            getProduct(event).setUrl(event.getText());
+            product.setUrl(event.getText());
         }
-        reply(session, event, getProduct(event).toRichMessage());
-        nextConversation(event);
-    }
-
-    @Controller(events = {EventType.DIRECT_MESSAGE})
-    public void confirm(WebSocketSession session, Event event) {
-        logger.info("Confirmar " + products.size());
-        logger.info("User null: " + (event.getUser() == null));
-        reply(session, event, getProduct(event).toRichMessage());
+        productRepository.save(product);
+        reply(session, event, new Message("Anuncio criado com sucesso"));
         stopConversation(event);
     }
+
+//    @Controller(events = {DIRECT_MESSAGE})
+//    public void confirm(WebSocketSession session, Event event) {
+//        logger.info("Confirmar " + products.size());
+//        logger.info("User null: " + (event.getUser() == null));
+//        reply(session, event, getProduct(event).toRichMessage());
+//        stopConversation(event);
+//    }
 
     private Product getProduct(Event event) {
         logger.info("Event userid:" + event.getUserId());
         logger.info("Products size: " + products.size());
         logger.info("User null: " + (event.getUser() == null));
         return (products.containsKey(event.getUserId()) ? products.get(event.getUserId()) : new Product().setSellerName(event.getUserId()));
-    }
-
-    private final void reply(WebSocketSession session, Event event, RichMessage reply) {
-        try {
-            if(reply.getChannel() == null && event.getChannelId() != null) {
-                reply.setChannel(event.getChannelId());
-            }
-
-            session.sendMessage(new TextMessage(toJSONString(reply)));
-            if(logger.isDebugEnabled()) {
-                logger.debug("Reply (RichMessage): {}", toJSONString(reply));
-            }
-        } catch (IOException var5) {
-            logger.error("Error sending event: {}. Exception: {}", event.getText(), var5.getMessage());
-        }
     }
 
     private String toJSONString(RichMessage richMessage) throws JsonProcessingException {
